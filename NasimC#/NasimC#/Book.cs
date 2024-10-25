6e1fd0c32576f06a3f5bf52f755151d4ab5e2d6d
Nasim Rana Feroz <feroznasimrana@gmail.com>
2024-10-25 20:17:54 +0800

    C#

---
+++ NasimC#/NasimC#/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NasimC_
+{
+    class Book
+    {
+        public string author;
+        public string name;
+        public string rating;
+
+        public Book(string aAuthor, string aName, string aRating)
+        {
+            author = aAuthor;
+            name = aName;
+            rating = aRating;
+        }
+    }
+    
+}
---
+++ NasimC#/NasimC#/Program.cs
@@ -0,0 +1,7 @@
+﻿using NasimC_;
+
+   Book book1 = new Book("Nasim", "Hope", "R");
+   Console.WriteLine(book1.name);
+
+
+
